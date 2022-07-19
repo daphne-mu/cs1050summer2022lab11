fbb2ab1695d6654131f7eb339c43c767a4e7576c
Daphne-MU <32230740+daphne-mu@users.noreply.github.com>
2022-07-19 11:20:04 -0500

    Update lab11.c

--- lab11.c
+++ lab11.c
@@ -9,8 +9,8 @@
 int main(int argc, char * argv[])
 {
 	// Uncomment the following line if you want random (different every time) fights
-	// SetSeed(0);
+	SetSeed(0);
 
 	// Uncomment the following line if you want to try a sample fight
-	// Fight("JimR",21,900,10,"5d6+5","5d12+5","Dr. Lawrence Ries",5,100,5,"1d6","N/A");
+	Fight("JimR",21,900,10,"5d6+5","5d12+5","Dr. Lawrence Ries",5,100,5,"1d6","N/A");
 }
